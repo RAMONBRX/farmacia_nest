1fcbbe923b869fd7f6b8c57f0753576bcadd1a2a
RAMONBRX <ramon.all98@hotmail.com>
2026-03-17 11:52:16 -0300

    ✨ New Feature: Construção do recurso produto com relacionamento com categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriaController } from './categoria/controllers/categoria.controller';
 import { CategoriaModule } from './categoria/categoria.module';
+import { ProdutoModule } from './produto/produto.module';
 
 
 @Module({
@@ -16,7 +17,8 @@ import { CategoriaModule } from './categoria/categoria.module';
       autoLoadEntities: true,
       synchronize: true
     }),
-    CategoriaModule
+    CategoriaModule,
+    ProdutoModule
   ],
   controllers: [],
   providers: [],
--- src/categoria/services/categoria.service.ts
+++ src/categoria/services/categoria.service.ts
@@ -11,7 +11,13 @@ export class CategoriaService {
     ) { }
 
     async findAll(): Promise<Categoria[]> {
-        return await this.categoriaRepository.find();
+        return await this.categoriaRepository.find(
+            {
+                relations: {
+                    produto: true
+                }
+            }
+        );
     }
 
     async findById(id: number): Promise<Categoria> {
@@ -19,6 +25,9 @@ export class CategoriaService {
         const categoria = await this.categoriaRepository.findOne({
             where: {
                 id
+            },
+            relations: {
+                produto: true
             }
         });
 
@@ -33,6 +42,9 @@ export class CategoriaService {
         return await this.categoriaRepository.find({
             where: {
                 nome: ILike(`%${nome}%`)
+            },
+            relations: {
+                produto: true
             }
         })
     }
